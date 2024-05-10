f22387088138159ca523129321953e67bf0f6794
JDML21 <161516253+JDML21@users.noreply.github.com>
2024-05-10 10:57:59 -0500

    arreglos 2

--- src/routes/AppNavigation.tsx
+++ src/routes/AppNavigation.tsx
@@ -1,36 +1,24 @@
-import React from "react"
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import StartScreen from "../views/StartScreen";
-<<<<<<< HEAD
 import LoginScreen from "../views/LoginScreen";
 import SignUpScreen from "../views/SignUpScreen";
 import LoginSelectionScreen from "../views/LoginSelectionScreen";
 import HomeScreen from "../views/HomeScreen";
-=======
->>>>>>> authContext
 
 const Stack = createNativeStackNavigator();
 
-export default function AppNavigation(){
-  return <NavigationContainer> 
-<<<<<<< HEAD
-            <Stack.Navigator initialRouteName="Start"
-            screenOptions={{
-                headerShown:false
-            }}>
-                <Stack.Screen name="Start" component={StartScreen} />
-                <Stack.Screen name="LoginSelection" component={LoginSelectionScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="SignUp" component={SignUpScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
-=======
-            <Stack.Navigator initialRouteName="Home"
-            screenOptions={{
-                headerShown:false
-            }}>
-                <Stack.Screen name="Home" component={StartScreen} />
->>>>>>> authContext
-            </Stack.Navigator>
+export default function AppNavigation() {
+  return (
+    <NavigationContainer> 
+      <Stack.Navigator initialRouteName="Start" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Start" component={StartScreen} />
+        <Stack.Screen name="LoginSelection" component={LoginSelectionScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
 }
