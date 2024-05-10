91ba0dde2d83a28cd26ab5b0e325c41208931644
JDML21 <161516253+JDML21@users.noreply.github.com>
2024-05-10 10:53:41 -0500

    pequeños arreglos

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
-import React from "react"
+import React from "react";
 import AppNavigation from "./src/routes/AppNavigation";
-<<<<<<< HEAD
-//import { AuthProvider } from "./src/context/AuthContext";
+import { AuthProvider } from "./src/context/AuthContext";
 
-export default function App(){
-  return <AuthProvider>
-             <AppNavigation />
-      </AuthProvider>
-=======
-//import { AuthProvider } from "./src/context/authContext/AuthContext";
-
-export default function App(){
+export default function App() {
   return (
+    <AuthProvider>
       <AppNavigation />
-)
->>>>>>> authContext
+    </AuthProvider>
+  );
 }
